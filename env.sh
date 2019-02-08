82121055668fbddf9ee4acdfc19eb2c6f0b69767
Dave Bush <davebush@gmail.com>
2019-02-08 06:47:02 -0500

    Fixed script to deal with multiple userpools and identity pools in the same account.
    Signed-off-by: Dave Bush <davebush@gmail.com>

--- env.sh
+++ env.sh
@@ -9,6 +9,25 @@ help ()
   echo "$0 -p p2c-dev"
 }
 
+search_json_array () { 
+
+  JSON_ARRAY=$1
+  STAGE=$2
+  KEY='.'$3
+  VALUE='.'$4
+
+  for row in $(echo $1 | jq -r '.[] | @base64'); do
+    _jq() {
+      echo ${row} | base64 --decode | jq -r ${1}
+    }
+
+
+    if echo "$(_jq $KEY)" | grep -q "$STAGE"; then
+      echo $(_jq $VALUE)
+    fi
+  done
+}
+
 while getopts ":hr:p:" option; do
   case "$option" in
     h)
@@ -31,17 +50,23 @@ while getopts ":hr:p:" option; do
   esac
 done
 
-USER_POOL_ID=$(aws --profile $PROFILE --region $DEFAULT_REGION cognito-idp  list-user-pools --max-results 1 | jq -r .UserPools[0].Id)
+#USER_POOL_ID=$(aws --profile $PROFILE --region $DEFAULT_REGION cognito-idp  list-user-pools --max-results 1 | jq -r .UserPools[0].Id)
 #USER_POOL_REGION=$(echo $USER_POOL_ID | cut -d'_' -f 1 )
-APP_CLIENT_ID=$(aws --profile $PROFILE --region $DEFAULT_REGION cognito-idp list-user-pool-clients --user-pool-id $USER_POOL_ID --max-results 1 | jq -r .UserPoolClients[0].ClientId)
-IDENTITY_POOL_ID=$(aws --profile $PROFILE --region $DEFAULT_REGION cognito-identity list-identity-pools --max-results 1 | jq -r .IdentityPools[0].IdentityPoolId)
+USERPOOLS=$(aws --profile $PROFILE --region $DEFAULT_REGION cognito-idp  list-user-pools --max-results 60 | jq -r .UserPools)
+USER_POOL_ID=$(search_json_array "$USERPOOLS" sandbox- Name Id)
+USERPOOL_CLIENTS=$(aws --profile $PROFILE --region $DEFAULT_REGION cognito-idp list-user-pool-clients --user-pool-id $USER_POOL_ID --max-results 60 | jq -r .UserPoolClients)
+USERPOOL_CLIENT_ID=$(search_json_array "$USERPOOL_CLIENTS" sandbox ClientName ClientId)
+#IDENTITY_POOL_ID=$(aws --profile $PROFILE --region $DEFAULT_REGION cognito-identity list-identity-pools --max-results 1 | jq -r .IdentityPools[0].IdentityPoolId)
+IDENTITY_POOLS=$(aws --profile $PROFILE --region $DEFAULT_REGION cognito-identity list-identity-pools --max-results 60 | jq -r .IdentityPools)
+IDENTITY_POOL_ID=$(search_json_array "$IDENTITY_POOLS" sandbox IdentityPoolName IdentityPoolId)
+#echo $IDENTITY_POOLS
 REST_API_ID=$(aws --profile $PROFILE --region $DEFAULT_REGION apigateway get-rest-apis | jq -r .items[0].id)
 
 echo "STAGE=$STAGE"
 echo "PROFILE=$PROFILE"
 echo "DEFAULT_REGION=$DEFAULT_REGION"
 echo "USER_POOL_ID=$USER_POOL_ID"
-echo "USER_POOL_REGION=$USER_POOL_REGION"
-echo "APP_CLIENT_ID=$APP_CLIENT_ID"
+echo "USER_POOL_REGION=$DEFAULT_REGION"
+echo "USERPOOL_CLIENT_ID=$USERPOOL_CLIENT_ID"
 echo "IDENTITY_POOL_ID=$IDENTITY_POOL_ID"
 echo "REST_API_ID=$REST_API_ID"
